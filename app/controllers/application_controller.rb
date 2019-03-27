2982d491e99f991d61203e629dca67fd15f1fdf0
kemega <kemega2@gmail.com>
2019-03-27 02:36:55 +0200

    fixed application_controller.rb identation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 	helper_method :current_user
 	
 	private
-
+	
 	def current_user
   	return nil unless session[:user_id]
   	@current_user ||= User.find_by(id: session[:user_id])
