352c37526306732a287edd71ad6a08ec58a152c3
kemega <kemega2@gmail.com>
2019-05-15 12:42:35 +0200

    removed powershell script

--- start.ps1
+++
@@ -1,3 +0,0 @@
-Set-Location c:\users\kemeg\desktop\dota_analyzer
-invoke-expression 'cmd /c start powershell -Command { docker-compose run web bundle exec crono RAILS_ENV=development}'
-docker-compose up
